0849721bcab3532272920b928bd854004a14a342
Chris Taylor <ctaylor@tactcomplabs.com>
2022-02-27 16:27:55 -0500

    added comments to clarify nofi communication functions

--- nofi/nofi.nim
+++ nofi/nofi.nim
@@ -50,9 +50,12 @@ proc nofi_wait*() : int =
     result = bindings.rofi_wait()
 
 proc nofi_put*[T : SomeNumber](dst : ptr UncheckedArray[T], dst_len : uint32, src : ptr UncheckedArray[T], src_len : uint32, id : uint32, flags : uint64) : int =
-    ## Asynchronous put; transfers byte in `src` in the current process virtual address space to
+   ## Asynchronous put; transfers byte in `src` in the current process virtual address space to
     ## process `id` at address `dst` in the destination. Users must check for completion or invoke
     ## nofi_wait. Do not modify the seq before the transfer terminates
+    ##
+    ## dst and src are both ptr UncheckedArray[T]'s that point/reference memory allocated with nofi_alloc
+    ## if dst and src do not point/reference nofi_alloc'd memory undefined behavior will occur.
     ##
     assert(dst_len == src_len)
     result = bindings.rofi_put(dst, src, T.sizeof * src_len, id, flags)
@@ -61,6 +64,9 @@ proc nofi_get*[T : SomeNumber](dst : ptr UncheckedArray[T], dst_len : uint32, sr
     ## Asynchronous get; transfers bytes in `src` in a remote process `id`'s virtual address space
     ## into the current process at address `dst`. Users must check for completion or invoke nofi_wait.
     ## Do not modify the seq before the transfer terminates
+    ##
+    ## dst and src are both ptr UncheckedArray[T]'s that point/reference memory allocated with nofi_alloc
+    ## if dst and src do not point/reference nofi_alloc'd memory undefined behavior will occur.
     ## 
     assert(dst_len == src_len)
     result = bindings.rofi_get(dst, src, T.sizeof * src_len, id, flags)
@@ -69,12 +75,18 @@ proc nofi_iput*[T : SomeNumber](dst : ptr UncheckedArray[T], dst_len : uint32, s
     ## Synchronous put; transfers byte in `src` in the current process virtual address space to
     ## process `id` at address `dst` in the destination process.
     ##
+    ## dst and src are both ptr UncheckedArray[T]'s that point/reference memory allocated with nofi_alloc
+    ## if dst and src do not point/reference nofi_alloc'd memory undefined behavior will occur.
+    ##
     assert(dst_len == src_len)
     result = bindings.rofi_put(dst, src, T.sizeof * src.len, id, flags)
 
 proc nofi_iget*[T : SomeNumber](dst : ptr UncheckedArray[T], dst_len : uint32, src : ptr UncheckedArray[T], src_len : uint32, id : uint32, flags : uint64) : int =
     ## Synchronous get; transfers bytes in `src` in a remote process `id`'s virtual address space
     ## into the current process at address `dst`.
+    ##
+    ## dst and src are both ptr UncheckedArray[T]'s that point/reference memory allocated with nofi_alloc
+    ## if dst and src do not point/reference nofi_alloc'd memory undefined behavior will occur.
     ## 
     assert(dst_len == src_len)
     result = bindings.rofi_get(dst, src, T.sizeof * src.len, id, flags)
@@ -82,10 +94,16 @@ proc nofi_iget*[T : SomeNumber](dst : ptr UncheckedArray[T], dst_len : uint32, s
 proc nofi_isend*[T : SomeNumber](id : uint32, src : ptr UncheckedArray[T], src_len : uint32, flags : uint64) : int =
     ## Synchronous transfer bytes from the current process at address `src` to process `id` 
     ##
+    ## dst and src are both ptr UncheckedArray[T]'s that point/reference memory allocated with nofi_alloc
+    ## if dst and src do not point/reference nofi_alloc'd memory undefined behavior will occur.
+    ##
     result = bindings.rofi_isend(id, src, T.sizeof*src_len, flags)
 
 proc nofi_irecv*[T : SomeNumber](id : uint32, src : ptr UncheckedArray[T], src_len : uint32, flags : uint64) : int =
     ## Synchronous transfer bytes from remote process `id` into current process at 'src' 
+    ##
+    ## dst and src are both ptr UncheckedArray[T]'s that point/reference memory allocated with nofi_alloc
+    ## if dst and src do not point/reference nofi_alloc'd memory undefined behavior will occur.
     ##
     result = bindings.rofi_irecv(id, src, T.sizeof*src_len, flags)
 
@@ -112,11 +130,17 @@ proc nofi_get_remote_address*[T : SomeNumber](src : ptr UncheckedArray[T], id : 
     ## symmetric in the SPMD application, only the offsets are; this function maps
     ## an address at 'src' in a current process with it's remote counterpart.
     ##
+    ## src is a ptr UncheckedArray[T] that points/references memory allocated with nofi_alloc
+    ## if src does not point/reference nofi_alloc'd memory undefined behavior will occur.
+    ##
     result = bindings.rofi_get_remote_address(src, id)
 
 proc nofi_get_local_address_from_remote_address*[T : SomeNumber](src : ptr UncheckedArray[T], id : uint64) : pointer =
     ## this function map the an address at 'src' on a remote process with the corresponding
     ## address in the local process.
+    ##
+    ## src is a ptr UncheckedArray[T] that points/references memory allocated with nofi_alloc
+    ## if src does not point/reference nofi_alloc'd memory undefined behavior will occur.
     ##
     result = bindings.rofi_get_local_address_from_remote_address(src, id)
 
